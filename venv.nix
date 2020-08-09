59f54cae846f46a4d50c97e5e7a9dae060092751
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-09 14:47:29 +0000

    fix "cannot refer to derivation outputs"

--- venv.nix
+++ venv.nix
@@ -10,11 +10,12 @@ stdenv.mkDerivation {
     name = venvName;
     buildInputs = buildInputs;
 
+    inherit pip requirements;
     builder = builtins.toFile "venv-builder.sh" ''
         source $stdenv/setup
 
         unset SOURCE_DATE_EPOCH
         mkdir -p $out
-        PIP_PREFIX=$out ${pip}/bin/pip install -r ${requirements}
+        PIP_PREFIX=$out $pip/bin/pip install -r $requirements
     '';
 }
