ff1ed10d8960433444abf99b155c8824068835d3
jali-clarke <jinnah.ali-clarke@outlook.com>
2020-08-09 14:44:20 +0000

    go straight for builder

--- venv.nix
+++ venv.nix
@@ -10,7 +10,9 @@ stdenv.mkDerivation {
     name = venvName;
     buildInputs = buildInputs;
 
-    installPhase = ''
+    builder = builtins.toFile "venv-builder.sh" ''
+        source $stdenv/setup
+
         unset SOURCE_DATE_EPOCH
         mkdir -p $out
         PIP_PREFIX=$out ${pip}/bin/pip install -r ${requirements}
